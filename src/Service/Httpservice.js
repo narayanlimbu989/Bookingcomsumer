b4ace802e5059e56bda9af8416cbfb64da2236d8
Narayan limbu <narayanlimbuofficial@.com>
2023-04-01 19:40:19 +0545

    update

--- src/Service/Httpservice.js
+++ src/Service/Httpservice.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const token = JSON.parse(localStorage.getItem("customer"))?.token;
-export const base_url = "https://bookingapi-8rjt.onrender.com/hotels/api";
+export const base_url = "https://bookingapi-8rjt.onrender.com";
 export const api = axios.create({
   baseURL: base_url,
   withCredentials: true,
